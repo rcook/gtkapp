5242d992c23eda802dbcca4b34fc9a1f34bca02d
Richard Cook <rcook@rcook.org>
2016-05-26 17:15:50 -0600

    Check that Mac OS X accelerators are enabled

--- src/OSXApp.hs
+++ src/OSXApp.hs
@@ -12,6 +12,7 @@ module OSXApp
     , initApp
     ) where
 
+import           Control.Monad
 import qualified Graphics.UI.Gtk as Gtk
 import qualified Graphics.UI.Gtk.OSX as OSX
 
@@ -20,6 +21,12 @@ import qualified Graphics.UI.Gtk.OSX as OSX
 initApp :: IO OSX.Application
 initApp = do
     app <- OSX.applicationNew
+
+    usesQuartzAccelerators <- OSX.applicationGetUseQuartzAccelerators app
+    unless usesQuartzAccelerators $ do
+        putStrLn "WARNING: applicationGetUseQuartzAccelerators returned False"
+        OSX.applicationSetUseQuartzAccelerators app True
+
     menuBar <- Gtk.menuBarNew
     OSX.applicationSetMenuBar app menuBar
     OSX.applicationReady app
